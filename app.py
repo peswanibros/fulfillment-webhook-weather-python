e37a100349655715cdfa9dc5bf55872bb8564287
Manohar Peswani <peswani.sunny121@gmail.com>
2017-12-12 02:40:42 +0530

    change to bitcoin price

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def webhook():
 def processRequest(req):
     if req.get("result").get("action") != "cryptoPrice":
         return {}
-    baseurl = "http://www.json-generator.com/api/json/get/cqlIdHfiuW?indent=2"
+    baseurl = "https://www.zebapi.com/api/v1/market/ticker/btc/inr"
     result = urlopen(baseurl).read()
     print(result)
     data = json.loads(result)
